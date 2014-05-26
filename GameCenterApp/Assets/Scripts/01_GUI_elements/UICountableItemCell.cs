f74d70d98e2d76eddb3803f5b62f3f2995f8117a
statsenko <e.a.statsenko@yandex.ru>
2014-05-26 19:12:38 +0300

    code refactoring

--- GameCenterApp/Assets/Scripts/01_GUI_elements/UICountableItemCell.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class UICountableItemCell: MonoBehaviour 
-{
-	public UILabel countLabel = null;
-	public UITexture iconTexture = null;
-	public UITexture backgroundTexture = null;
-}
